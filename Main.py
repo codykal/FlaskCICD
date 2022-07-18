db5f2624ff08d68f28d6c2e7de6f59e79e2516aa
codykal <codyakal@gmail.com>
2022-07-18 15:18:53 -0700

    Fixed Stuff

--- Main.py
+++ Main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
-app = Flask(__main__)
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Hello! This is the main page <h1>HELLO<h1>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
 
 if __name__ == "__main__":
     app.run()
